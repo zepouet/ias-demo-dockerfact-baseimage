d4c557b1db1dbdc69e9339dc4b52ca04444c63db
Nicolas Muller <zepouet@gmail.com>
2022-01-16 19:58:53 +0100

    fix: test

--- .github/workflows/tag-as-stable.sh
+++ .github/workflows/tag-as-stable.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+docker pull baseartifacts.azurecr.io/php:8.1-test
 docker tag baseartifacts.azurecr.io/php:8.1-test baseartifacts.azurecr.io/php:8.1-stable
 docker push baseartifacts.azurecr.io/php:8.1-stable
