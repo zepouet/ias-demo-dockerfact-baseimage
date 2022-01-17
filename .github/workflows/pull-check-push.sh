e79e87751d031380f19fc56f7b3ac7889d19444f
Nicolas Muller <zepouet@gmail.com>
2022-01-17 10:22:22 +0100

    fix: test

--- .github/workflows/pull-check-push.sh
+++ .github/workflows/pull-check-push.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-REGISTRY=$1
-IMAGE=$2
-
 docker pull baseartifacts.azurecr.io/php:8.1-test || true
 docker pull php:8.1
 DIGEST1=$(docker images --no-trunc --quiet baseartifacts.azurecr.io/php:8.1-test)
 DIGEST2=$(docker images --no-trunc --quiet php:8.1)
-echo "DIGEST1="$DIGEST1
-echo "DIGEST2="$DIGEST2
+echo "DIGEST1(baseartifacts.azurecr.io/php:8.1-test)="$DIGEST1
+echo "DIGEST2(php:8.1)="$DIGEST2
 if [ "$DIGEST1" != "$DIGEST2" ]; then
   echo DIFF DETECTED. SHOW MUST GO ON
   docker rmi baseartifacts.azurecr.io/php:8.1-test || true
