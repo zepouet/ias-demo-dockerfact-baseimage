db6b185df56a2dfdc1b4884f984646e4ddc8b426
Nicolas Muller <zepouet@gmail.com>
2022-01-17 10:56:34 +0100

    fix: test

--- .github/workflows/pull-check-push.sh
+++ .github/workflows/pull-check-push.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-docker pull baseartifacts.azurecr.io/php:8.1-test || true
-docker pull php:8.1
-DIGEST1=$(docker images --no-trunc --quiet baseartifacts.azurecr.io/php:8.1-test)
-DIGEST2=$(docker images --no-trunc --quiet php:8.1)
-echo "DIGEST1(baseartifacts.azurecr.io/php:8.1-test)="$DIGEST1
-echo "DIGEST2(php:8.1)="$DIGEST2
+REGISTRY=$1
+IMAGE=$2
+
+docker pull ${REGISTRY}/${IMAGE}-test || true
+docker pull ${IMAGE}
+DIGEST1=$(docker images --no-trunc --quiet ${REGISTRY}/${IMAGE}-test)
+DIGEST2=$(docker images --no-trunc --quiet ${IMAGE})
+echo "DIGEST1(${REGISTRY}/${IMAGE}-test)="$DIGEST1
+echo "DIGEST2(${IMAGE})="$DIGEST2
 if [ "$DIGEST1" != "$DIGEST2" ]; then
   echo DIFF DETECTED. SHOW MUST GO ON
-  docker rmi baseartifacts.azurecr.io/php:8.1-test || true
-  docker tag php:8.1 baseartifacts.azurecr.io/php:8.1-test
-  docker push baseartifacts.azurecr.io/php:8.1-test
-  echo PUSHED for test
+  docker rmi ${REGISTRY}/${IMAGE}-test || true
+  docker tag ${IMAGE} ${REGISTRY}/${IMAGE}-test
+  docker push ${REGISTRY}/${IMAGE}-test
+  echo PUSHED for test for ${IMAGE}
   echo ::set-output name=image::updated
 else
-  echo SKIPPED found for php:8.1
+  echo SKIPPED found for ${IMAGE}
   echo ::set-output name=image::skipped
 fi
