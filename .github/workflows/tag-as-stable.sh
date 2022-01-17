1b6eaf98b1138d79849388f01972573610bd285e
Nicolas Muller <zepouet@gmail.com>
2022-01-17 22:17:18 +0100

    fix: test

--- .github/workflows/tag-as-stable.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-REGISTRY=$1
-IMAGE=$2
-
-docker pull ${REGISTRY}/${IMAGE}-test
-docker tag ${REGISTRY}/${IMAGE}-test ${REGISTRY}/${IMAGE}-stable
-docker push ${REGISTRY}/${IMAGE}-stable
